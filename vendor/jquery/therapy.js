732f17c82a0d18c8b2810866d1a5f9afcf0156ac
theallisonbrett <theallisonbrett@gmail.com>
2017-06-01 22:08:50 -0700

    Update quote on client's request

--- vendor/jquery/therapy.js
+++ vendor/jquery/therapy.js
@@ -1,7 +1,9 @@
  $(document).ready(function() {
    $("#h1").hide().fadeIn(1000);
-   $("#h2").hide().fadeIn(2000)
+   $("#h2").hide().fadeIn(2000);
    $("blockquote").hide().fadeIn(3000);
+
+
 
 
 
