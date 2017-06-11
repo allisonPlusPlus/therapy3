cf27774479e784037d1d1f8265c7dea8c279808b
theallisonbrett <theallisonbrett@gmail.com>
2017-06-11 10:38:20 -0700

    Tidy up fadeIn animations

--- vendor/jquery/therapy.js
+++ vendor/jquery/therapy.js
@@ -1,14 +1,10 @@
- $(document).ready(function() {
-   $("#h1").hide().fadeIn(1000);
-   $("#h2").hide().fadeIn(2000);
-   $("blockquote").hide().fadeIn(3000);
-   $(".profile").hide().fadeIn(2000);
-   $(".aboutme").hide().fadeIn(3000);
-   $(".ul").hide().fadeIn(3000);
-   $("p").hide().fadeIn(2000);
+function animate() {
+  $(".a").hide().fadeIn(1000);
+  $(".b").hide().fadeIn(2000);
+  $(".c").hide().fadeIn(3000);
+}
 
 
-
-
-
-     });
+$(function() {
+  animate();
+});
